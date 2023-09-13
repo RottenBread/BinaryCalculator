a7b19a2a0d1111ed8892566d9f18b12f55bc555f
Junhyeok Moon <mig070@naver.com>
2023-09-13 14:09:30 +0900

    Update BinaryCalculator.py

--- BinaryCalculator.py
+++ BinaryCalculator.py
@@ -2,7 +2,9 @@ from NumToBinary import NumToBinary
 
 def BinaryCalculator(number1, number2):
     try:
-        binary1, binary2 = NumToBinary(number1), NumToBinary(number2)    
+        binary1, binary2 = NumToBinary(number1), NumToBinary(number2)
+        binary1 = [[binary1[i:i+4]] for i in range(0, len(binary1), 4)]
+        binary2 = [[binary2[i:i+4]] for i in range(0, len(binary2), 4)]
         return binary1, binary2
 
     except Exception as er:
