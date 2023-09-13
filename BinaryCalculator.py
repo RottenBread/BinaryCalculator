6e1a17d9c67c4e9ff13b61a5750f0d422a10a192
Junhyeok Moon <mig070@naver.com>
2023-09-14 01:01:54 +0900

    Update BinaryCalculator.py

--- BinaryCalculator.py
+++ BinaryCalculator.py
@@ -3,6 +3,10 @@ from DivideBinary import DivideBinary
 def BinaryCalculator(number1, number2):
     try:
         number1, number2 = DivideBinary(number1), DivideBinary(number2)
-        return number1, number2
+        for i, n in zip(number1, number2):
+            i, n = int(i), int(n)
+            i, n = '{0:04d}'.format(i), '{0:04d}'.format(n)
+            print(i, n)
+
     except Exception as er:
         print(f"BinaryCalculator : {er}")
